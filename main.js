e3f4891b887901c9281f01053ce16a515042eeeb
royc67 <royc67@gmail.com>
2020-11-30 16:06:18 +0200

    generate game function

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("hello world");
+const generateGame = (boardSize = 10, mineCount = 15) => {
+  const grid = Array.from({ length: boardSize }, () =>
+    Array.from({ length: boardSize }, () => "")
+  );
+
+  let mineX, mineY;
+  let minesPosition = [];
+  while (mineCount > 0) {
+    do {
+      [mineX, mineY] = [
+        Math.floor(Math.random() * boardSize),
+        Math.floor(Math.random() * boardSize),
+      ];
+    } while (grid[mineX][mineY] === "M");
+
+    minesPosition.push([mineX, mineY]);
+    grid[mineX][mineY] = "M";
+
+    mineCount--;
+  }
+  grid.forEach((row, x) => {
+    row.forEach((square, y) => {
+      if (square === "M") return;
+      const counter = countMines([x, y], grid);
+      counter && (grid[x][y] = counter);
+    });
+  });
+  return grid;
+};
